37f520fc14a33479a41b4f5dc1c5172e1a26ed8c
hannanic <hannanic@kth.se>
2025-04-16 10:38:39 +0200

    removed flask

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
 import json
-from flask import Flask, request, jsonify
 from scheduler import schedule_tasks
     
 app = FastAPI()
@@ -12,8 +11,6 @@ app = FastAPI()
 def root():
     return {"message": "FastAPI is working!"}
 
-
-app = Flask(__name__)
 
 
 @app.route('/schedule', methods=['POST'])
