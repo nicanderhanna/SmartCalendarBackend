80ee545b8cbf30ba4eaca350011cfad968ad27d6
hannanic <hannanic@kth.se>
2025-05-07 10:31:19 +0200

    Co-authored-by: Joel Friis <Joelfriis@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 pip install -r requirements.txt
 
 # Starta FastAPI med uvicorn på Render-vänligt sätt
-# uvicorn backend.app.main:app --host=0.0.0.0 --port=$PORT
+uvicorn backend.app.main:app --host=0.0.0.0 --port=$PORT
 
-uvicorn backend.app.main:app --reload
+#uvicorn backend.app.main:app --reload
